ea79a87bbf64e98cc9bb77f86647438b33fb1db2
BryantDavis1986 <bryant.davis@hotmail.com>
2020-05-22 13:14:33 -0700

    first commit

---
+++ finalExamQuestions/1-serverReview/serverReview1.test.js
@@ -0,0 +1,49 @@
+/* ------------------------------------------------------------------------------------------------
+
+Write a function named sayHello, that sends the message 'Hello from the back-end' when a user hits the `/hello` route.
+
+------------------------------------------------------------------------------------------------ */
+
+// Express sever here
+const createServer = () => {
+  const express=require('express');
+  const app=express();
+  app.get('/hello', sayHello);
+
+  var server = app.listen(3301, function () {
+    var port = server.address().port;
+    console.log('Example app listening at port', port);
+  });
+  return server;
+};
+
+
+function sayHello(request, response){
+  //Solution code here...
+}
+
+describe('Testing challenge', () => {
+
+  const request = require('supertest');
+
+  let server;
+
+  beforeEach(function () {
+    server = createServer();
+  });
+
+  afterEach(function () {
+    server.close();
+  });
+
+  test('responds to /hello', function testSlash(done) {
+    request(server)
+      .get('/hello')
+      .expect(200, done);
+  });
+  test('404 everything else', function testPath(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
+  });
+});
